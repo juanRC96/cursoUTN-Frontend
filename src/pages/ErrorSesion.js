9b093c664afe3e0e6ff0ccbad9b43ab38a191f71
juani <juanicuesta1996@gmail.com>
2022-08-29 11:05:13 -0300

    commit

--- src/pages/ErrorSesion.js
+++ src/pages/ErrorSesion.js
@@ -15,13 +15,6 @@ export default function ErrorSesion(props){
                 <Button as={Link} to="/login">Iniciar sesión</Button>
                 </>
             }
-                        {
-                error === "sesioniniciada" &&
-                <>
-                <h3>Ya hay una sesión iniciada</h3>
-                <Button as={Link} to="/">Volver al home</Button>
-                </>
-            }
         </div>
         </div>
     )
