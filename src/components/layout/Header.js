01bca902b6d91b2d120411b4d122ddb594fbd2ac
juani <juanicuesta1996@gmail.com>
2022-08-29 11:14:20 -0300

    commit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "../../styles/components/layout/Header.css"
-import { Link } from "react-router-dom";
 
 const Header = (props) => {
     return(
         <header>
             <div className="holder">
-                <a as={Link} to="/"><img src="images/newspaper.png" width="100" alt="TecnoNews"/></a>
+                <img src="images/newspaper.png" width="100" alt="TecnoNews"/>
                 <h1>TecnoNews</h1>
             </div>
         </header>
